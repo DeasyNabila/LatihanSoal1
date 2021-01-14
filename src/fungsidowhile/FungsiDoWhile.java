8c7d09136b46afc78b887adccae12aa778547705
DeasyNabila <deasy_nabila_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 20:49:21 +0700

    Latihan Soal 1

---
+++ src/fungsidowhile/FungsiDoWhile.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fungsidowhile;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class FungsiDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil = 1;
+        do{
+        //statement
+        System.out.println(bil+" ");
+        bil++;
+        }while (bil<=10);
+        }
+    }
+    }
+    
+}
